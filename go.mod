b0effda4c4236f56a633492f8f810c51c48645ee
Asher Foa <asher@userclouds.com>
2025-02-19 11:22:26 -0500

    authz connectivity sample

---
+++ go.mod
@@ -0,0 +1,14 @@
+module hello-world
+
+go 1.23
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-http-utils/headers v0.0.0-20181008091004-fed159eddc2a // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/redis/go-redis/v9 v9.0.5 // indirect
+	userclouds.com v1.6.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"userclouds.com/authz"
+	"userclouds.com/infra/jsonclient"
+	"userclouds.com/infra/pagination"
+)
+
+func runAuthZ() {
+	ctx := context.Background()
+	clientID := os.Getenv("USERCLOUDS_CLIENT_ID")
+	clientSecret := os.Getenv("USERCLOUDS_CLIENT_SECRET")
+	region := os.Getenv("USERCLOUDS_REGION")
+	if region == "" {
+		region = "eu-west-1"
+	}
+	tenantHost := "usercloudstests-connectivity-tests.tenant.userclouds.com"
+	tenantURL := fmt.Sprintf("https://%s", tenantHost)
+	endpointURL := fmt.Sprintf("https://aws-%s-eks.userclouds.com", region)
+	log.Printf("Using URL: %s", endpointURL)
+	if clientID == "" || clientSecret == "" {
+		log.Fatal("missing one or more required environment variables: USERCLOUDS_CLIENT_ID, USERCLOUDS_CLIENT_SECRET")
+	}
+
+	ts, err := jsonclient.ClientCredentialsForURL(tenantURL, clientID, clientSecret, nil)
+	if err != nil {
+		log.Fatalf("Error getting token. check values of provided client id, client secret and tenant url : %s", err)
+	}
+	client, err := authz.NewClient(endpointURL, authz.BypassCache(), authz.JSONClient(ts), authz.JSONClient(jsonclient.HeaderHost(tenantHost)))
+
+	if err != nil {
+		log.Fatalf("Error creating authz client: %s", err)
+	}
+	edges := enumerateEdges(ctx, client)
+	for i, edge := range edges {
+		_, err := client.GetObject(ctx, edge.SourceObjectID)
+		if err != nil {
+			log.Fatalf("Error getting source object for edge %d: %s", i, err)
+		}
+		_, err = client.GetObject(ctx, edge.TargetObjectID)
+		if err != nil {
+			log.Fatalf("Error getting target object for edge %d: %s", i, err)
+		}
+		et, err := client.GetEdgeType(ctx, edge.EdgeTypeID)
+		if err != nil {
+			log.Fatalf("Error getting edge type for edge %d: %s", i, err)
+		}
+		log.Printf("Got objects for edge %d: %v [type: %v]", i, edge.ID, et.TypeName)
+	}
+}
+
+func enumerateEdges(ctx context.Context, client *authz.Client) []authz.Edge {
+	cursor := pagination.CursorBegin
+	edges := make([]authz.Edge, 0)
+	for {
+		es, err := client.ListEdges(ctx, authz.Pagination(pagination.StartingAfter(cursor)))
+		if err != nil {
+			log.Fatalf("Error listing edges: %s", err)
+		}
+		edges = append(edges, es.Data...)
+		if !es.HasNext {
+			break
+		}
+		log.Printf("Got %d edges, next: %s", len(edges), es.Next)
+		cursor = es.Next
+	}
+	log.Printf("Got %d edges", len(edges))
+	return edges
+}
+
+func main() {
+	runAuthZ()
+
+}
